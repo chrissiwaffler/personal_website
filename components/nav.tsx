81fafb3ef245605b4929b2fefc90ca41d44bc4e8
Christoph Waffler <waffler.christoph@gmail.com>
2024-10-13 20:08:41 +0200

    fix: padding navbar

--- components/nav.tsx
+++ components/nav.tsx
@@ -12,7 +12,7 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="-ml-[12px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex flex-row items-center justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
