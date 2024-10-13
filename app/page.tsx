b1aaf0c7faeb310b7ab1b3a54092fa27649b3c3e
Christoph Waffler <waffler.christoph@gmail.com>
2024-10-13 19:55:08 +0200

    fix: build error

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ export default function Home() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">hi!</h1>
-      <p className="mb-4">
-        currently i'm studying computer science at tum.
-        <br />i love inventing cool things.
+      <p className="mb-4 whitespace-pre-line">
+        {`currently i'm studying computer science at tum.
+          i love inventing cool things.
+        `}
       </p>
     </section>
   );
